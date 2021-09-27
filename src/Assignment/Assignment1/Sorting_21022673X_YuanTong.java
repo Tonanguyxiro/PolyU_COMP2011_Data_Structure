57895825f4f8a5f9e014ea1c3e147a716662f566
tongyuan <11810818@mail.sustech.edu.cn>
2021-09-27 16:31:54 +0800

    Finish code

--- src/Assignment/Assignment1/Sorting_21022673X_YuanTong.java
+++ src/Assignment/Assignment1/Sorting_21022673X_YuanTong.java
@@ -1,59 +1,60 @@
-package Lecture.Assignment1_template;
-import java.util.ArrayList;
+package Assignment.Assignment1;
+// Remember to move to finally
+
 import java.util.Arrays;
-import java.util.List;
 
 /**
- * 
- * @author Yixin Cao (September 11, 2021)
+ *
+ * @author Tong Yuan (September 27, 2021)
  *
  * A better implementation of insertion sort.
- * 
+ *
  * The ith major iteration of insertion sort puts element a[i] to its correct position.
  * Your algorithm should use **binary search** to find the position of the first element
  * that is greater than (> not >=) a[i], or i if there does exist such an element.
- * 
+ *
  * Read the {@code Element} class carefully before you start.
  * It uses an extra field *originalPos* to store the index of this element in the input.
  * When an element is output, originalPos is printed in parentheses.
- * 
+ *
  * If your implementation is correct, elements of the same value should respect their original order,
- * e.g., for input {1.25, 0, 1.25, 2.5, 10, 2.5, 1.25, 5, 2.5}, the output should be  
+ * e.g., for input {1.25, 0, 1.25, 2.5, 10, 2.5, 1.25, 5, 2.5}, the output should be
  * [0.0 (1), 1.25 (0), 1.25 (2), 1.25 (6), 2.5 (3), 2.5 (5), 2.5 (8), 5.0 (7), 10.0 (4)].
  */
-public class Sorting_12345678d_TangTszkei { // Please change!
-	/*
-	 * Each element has a double value and the original position in the input array. 
-	 */
-	static class Element {
-	    private int originalPos;
-	    double value;
-	    public Element(int i, double v) {
-	        originalPos = i; 
-	        value = v;
-	    }
-	    public String toString() {
-	        return (String.valueOf(value)) + " (" + String.valueOf(originalPos) + ")";
-	    }
-	}
+
+public class Sorting_21022673X_YuanTong { // Please change!
+    /*
+     * Each element has a double value and the original position in the input array.
+     */
+    static class Element {
+        private int originalPos;
+        double value;
+        public Element(int i, double v) {
+            originalPos = i;
+            value = v;
+        }
+        public String toString() {
+            return (String.valueOf(value)) + " (" + String.valueOf(originalPos) + ")";
+        }
+    }
 
     /**
      * VERY IMPORTANT.
-     * 
+     *
      * I've discussed this question with the following students:
-     *     1. 
-     *     2. 
-     *     3. 
-     *     ... 
-     * 
+     *     1.
+     *     2.
+     *     3.
+     *     ...
+     *
      * I've sought help from the following Internet resources and books:
-     *     1. 
-     *     2. 
-     *     3. 
-     *     ... 
-     * 
-     * Running time: O(   ).   
-     */ 
+     *     1.
+     *     2.
+     *     3.
+     *     ...
+     *
+     * Running time: O(   ).
+     */
     public static void insertionSort(Element[] a) {
         if(a.length == 0){
             System.out.println("Oops! No element in a!");
@@ -61,18 +62,20 @@ public class Sorting_12345678d_TangTszkei { // Please change!
         else {
             int start, end, mid = 0;
             int n = a.length;
-            List<Element> out = new ArrayList<>();
-            out.add(a[0]);
+//            List<Element> out = new ArrayList<>();
+//            out.add(a[0]);
+            Element[] out = new Element[n];
+            out[0] = a[0];
             for (int i = 1; i < n; i++){
                 start = 0; end = i - 1; // Search region
                 mid = (int) Math.ceil(0.5*(start+end));
-                if (a[i].value >= out.get(end).value){
-                    out.add(a[i]);
+                if (a[i].value >= out[end].value){
+                    out[i] = a[i];
                     continue;
                 }
                 else {
                     while (!(mid == end)){ // insert
-                        if (a[i].value < out.get(mid).value){
+                        if (a[i].value < out[mid].value){
                             end = mid;
                         }
                         else {
@@ -80,31 +83,32 @@ public class Sorting_12345678d_TangTszkei { // Please change!
                         }
                         mid = (int) Math.ceil(0.5*(start+end));
                     }
-                    out.add(mid, a[i]);
-                }
-                System.out.println("Interaction " + i + Arrays.toString(out.toArray()));
-            }
 
-            for (int i = 0; i < a.length; i++) {
-                a[i] = out.get(i);
+                    for (int j = mid+1; j < n; j++) {
+                        out[j] = out[j-1];
+                    }
+                    out[mid] = a[mid];
+                }
+                System.out.println("Interaction " + i + Arrays.toString(out));
             }
+            a = out;
         }
 
     }
-    
+
     // The original insertion sort is copied for your reference.
     public static void insertionSort(int[] a) {
         int i, j, key, n = a.length;
         for (i = 1; i < n; i++) {
             key = a[i];
             for (j = i - 1; j >= 0; j--) {
-                if (a[j] <= key) break;     
+                if (a[j] <= key) break;
                 a[j + 1] = a[j];
             }
             a[j + 1] = key;
         }
     }
-    
+
     // The binary search algorithm is copied for your reference.
     public static int binarySearch(int[] a, int key) {
         int n = a.length;
@@ -113,18 +117,18 @@ public class Sorting_12345678d_TangTszkei { // Please change!
             int mid = low + (high - low) / 2;
             if (a[mid] == key) return mid;
             else if (a[mid] > key) high = mid - 1;
-            else if (a[mid] < key) low = mid + 1; 
+            else if (a[mid] < key) low = mid + 1;
         }
         return -1;
     }
-    
+
     public static void main(String[] args) {
         double input[] = {1.25, 0, 1.25, 2.5, 10, 2.5, 1.25, 5, 2.5}; // try different inputs.
         int n = input.length;
-        Element[] a = new Element[n];
-        for (int i = 0; i < input.length; i++) 
-            a[i] = new Element(i, input[i]);
-        
+        Sorting_21022673X_YuanTong.Element[] a = new Sorting_21022673X_YuanTong.Element[n];
+        for (int i = 0; i < input.length; i++)
+            a[i] = new Sorting_21022673X_YuanTong.Element(i, input[i]);
+
         System.out.println("Original: " + Arrays.toString(a));
         insertionSort(a);
         System.out.println("After sorted: " + Arrays.toString(a));
